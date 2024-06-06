f41163dffb6ffd0767f88c87936326df529edbc9
Brandon <45514686+Boom123bam@users.noreply.github.com>
2024-06-06 13:41:13 +0100

    Add day 4

---
+++ 04/tempCodeRunnerFile.py
@@ -0,0 +1,7 @@
+
+# num_sleeping_days = {}
+# for i, line in enumerate(parsed_lines):
+#     [year, month, day, hour, minute, action, id] = line
+#     if action == "begins shift":
+#         current_id = id
+#     elif action == "falls asleep":
