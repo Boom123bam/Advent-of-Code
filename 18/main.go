f20f893a8ea0fb8cfec43febab98611e3cef142c
Brandon <45514686+Boom123bam@users.noreply.github.com>
2024-06-11 10:49:29 +0100

    add day 18

--- 18/main.go
+++ 18/main.go
@@ -6,6 +6,27 @@ import (
 	"os"
 )
 
+type Parser struct {
+	pos           int
+	input         string
+	stickynessMap map[byte]int
+}
+
+type Expression interface {
+	expression()
+	String() string
+}
+
+type IntExpression struct {
+	Value int
+}
+
+type InfixExpression struct {
+	left  Expression
+	right Expression
+	infix byte
+}
+
 func main() {
 	file, err := os.Open("18/input.txt")
 	// file, err := os.Open("18/test.txt")
@@ -15,55 +36,114 @@ func main() {
 	defer file.Close()
 	scanner := bufio.NewScanner(file)
 
-	sum := 0
+	sum1 := 0
+	p1 := Parser{stickynessMap: map[byte]int{
+		0:   0,
+		'*': 1,
+		'+': 1,
+	}}
+	sum2 := 0
+	p2 := Parser{stickynessMap: map[byte]int{
+		0:   0,
+		'*': 1,
+		'+': 2,
+	}}
+
 	for scanner.Scan() {
-		sum += eval(scanner.Text())
+		p1.input, p1.pos = scanner.Text(), 0
+		sum1 += eval(p1.parseExpression(0))
+
+		p2.input, p2.pos = scanner.Text(), 0
+		sum2 += eval(p2.parseExpression(0))
 	}
-	fmt.Println(sum)
+
+	fmt.Println(sum1)
+	fmt.Println(sum2)
 }
 
-func eval(input string) int {
-	result := 0
-	operator := byte('+')
-	for i := 0; i < len(input); i++ {
-		char := input[i]
-		switch char {
-		case ' ':
+func (i IntExpression) expression()   {}
+func (o InfixExpression) expression() {}
+
+func (i IntExpression) String() string {
+	return fmt.Sprint(i.Value)
+}
+func (o InfixExpression) String() string {
+	return fmt.Sprintf("(%s %c %s)", o.left.String(), o.infix, o.right.String())
+}
+
+func (p *Parser) parseExpression(leftStickyness int) Expression {
+	var left Expression
+	if p.currentToken() == '(' {
+		left = p.parseBracedExpression()
+	} else {
+		left = IntExpression{Value: int(p.currentToken() - '0')}
+	}
+
+	if p.peekToken() == 0 || p.peekToken() == ')' {
+		return left
+	}
+	rightStickyness := p.stickynessMap[p.peekToken()]
+
+	for p.peekToken() != 0 && p.peekToken() != ')' && rightStickyness > leftStickyness {
+		p.advance()
+		left = p.parseInfixExpression(left)
+		rightStickyness = p.stickynessMap[p.peekToken()]
+	}
+	return left
+}
+
+func (p *Parser) parseBracedExpression() Expression {
+	p.advance()
+	e := p.parseExpression(-1)
+	p.advance()
+	return e
+}
+
+func (p *Parser) parseInfixExpression(left Expression) InfixExpression {
+	infix := p.currentToken()
+	leftStickyness := p.stickynessMap[infix]
+	p.advance()
+	return InfixExpression{
+		left:  left,
+		infix: infix,
+		right: p.parseExpression(leftStickyness),
+	}
+}
+
+func (p *Parser) currentToken() byte {
+	return p.input[p.pos]
+}
+
+func (p *Parser) peekToken() byte {
+	if p.pos >= len(p.input)-1 {
+		return 0
+	}
+	pos := p.pos + 1
+	for ; p.input[pos] == ' '; pos++ {
+	}
+	return p.input[pos]
+}
+
+func (p *Parser) advance() bool {
+	if p.pos >= len(p.input)-1 {
+		return false
+	}
+	p.pos++
+	for ; p.input[p.pos] == ' '; p.pos++ {
+	}
+	return true
+}
+
+func eval(e Expression) int {
+	switch e := e.(type) {
+	case IntExpression:
+		return e.Value
+	case InfixExpression:
+		switch e.infix {
 		case '+':
-			operator = char
+			return eval(e.left) + eval(e.right)
 		case '*':
-			operator = char
-		default:
-			var operand int
-			if char == '(' {
-				start := i
-				i = getEndBracket(input, i)
-				operand = eval(input[start+1 : i])
-			} else {
-				operand = int(char - '0')
-			}
-			if operator == '+' {
-				result += operand
-			} else {
-				result *= operand
-			}
-		}
-	}
-	return result
-}
-
-func getEndBracket(input string, startBracketIndex int) int {
-	nestCount := 1
-	i := startBracketIndex + 1
-	for ; i < len(input); i++ {
-		switch input[i] {
-		case '(':
-			nestCount++
-		case ')':
-			nestCount--
-		}
-		if nestCount == 0 {
-			return i
+			return eval(e.left) * eval(e.right)
 		}
 	}
 	return -1
